87d1808ccbb0772a45cf79040e769ecaff2d2b08
Meredith Espinosa <goclonefilms@gmail.com>
2017-08-31 22:15:42 -0700

    Testing out message sending

---
+++ src/main/java/space/bbkr/aura/item/ItemNote.java
@@ -0,0 +1,32 @@
+package space.bbkr.aura.item;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.item.Item;
+import space.bbkr.aura.Aura;
+
+public class ItemNote extends ItemBase {
+
+
+    protected String name;
+
+    public ItemNote(String name) {
+        super(name);
+        setMaxStackSize(1);
+        setCreativeTab(Aura.creativeTab);
+    }
+
+
+    public EnumActionResult onItemUse(EntityPlayer p, World w, BlockPos pos, EnumHand h, EnumFacing f, float x, float y, float z) {
+        p.sendMessage(new TextComponentTranslation("aura.note.start"));
+        return EnumActionResult.SUCCESS;
+    }
+
+
+}
--- src/main/java/space/bbkr/aura/item/ModItems.java
+++ src/main/java/space/bbkr/aura/item/ModItems.java
@@ -3,16 +3,19 @@ package space.bbkr.aura.item;
 import net.minecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraft.creativetab.CreativeTabs;
+import space.bbkr.aura.Aura;
 
 public class ModItems {
 
+    public static ItemNote testNote = new ItemNote("test_note");
+
     public static void register(IForgeRegistry<Item> registry) {
         registry.registerAll(
-
+            testNote
         );
     }
 
     public static void registerModels() {
-
+        testNote.registerItemModel();
     }
 }
